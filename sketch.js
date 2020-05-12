f2107058be75492e38f10905c6bef92642d84f53
dschounz <61173807+dschounz@users.noreply.github.com>
2020-05-12 10:24:37 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,12 +18,12 @@ function keyTyped(){
   if(key== 's'){
     //save this image
     saveCanvas('fileName', 'png');
-  } else if (key === 'd'){
-    //display image
-    for(let i=0, i < array.length; i++){
-      line(array[i][0], array[i][1],array[i+1][0], array[i+1][1] )
-
-    }
-  }
+  // } else if (key === 'd'){
+  //   //display image
+  //   for(let i=0, i < array.length; i++){
+  //     line(array[i][0], array[i][1],array[i+1][0], array[i+1][1] )
+  //
+  //   }
+  // }
   return false;
 }
